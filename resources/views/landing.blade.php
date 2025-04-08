31cba2d54706e4b07deb4a2dc15269209f8f01a6
Leunhart <149126504+Leunhart@users.noreply.github.com>
2025-04-08 20:39:17 +0700

    Update landing.blade.php
    nambahin button get started hrefnya diganti dari "#" ke "/form"

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -79,7 +79,7 @@
               <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Sistem Pakar Deteksi Penyakit Ginjal</h1>
               <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8"> Kelompok 7 (Fuzzy Logic)</p>
               <div class="mt-10 flex items-center justify-center gap-x-6">
-                <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+                <a href="/form" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a> <!-- href menjadi /form -->
               </div>
             </div>
           </div>
