d47431c4509ab2c98277a45a5beac6c6290f46f5
Rohman <103978344+MohammadRohman@users.noreply.github.com>
2025-05-31 14:54:28 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,14 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', // <--- Add this line
+        port: 5173, // Ensure this is the port your Vite server runs on
+        // Optionally, if you're using HTTPS locally:
+        // https: true,
+        hmr: {
+            host: 'localhost', // For HMR, keep localhost or your specific IP
+            clientPort: 5173,
+        },
+    },
 });
