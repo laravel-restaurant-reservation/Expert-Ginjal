1e23cdb0afb3f61f1d2378daaa867da0047df58e
Rohman <103978344+MohammadRohman@users.noreply.github.com>
2025-06-05 13:03:59 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,3 @@ Route::get('diagnose', function () {
 Route::post('diagnose/proses', [DiagnoseController::class, 'proses'])->name('diagnose.proses');
 
 Route::get('/hasil/export', [DiagnoseController::class, 'exportPDF'])->name('hasil.export');
-
-Route::get('/test-upload', function () {
-    $pdfContent = file_get_contents(public_path('dummy.pdf'));
-    $path = 'reports/test_' . now()->timestamp . '.pdf';
-
-    Storage::disk('s3')->put($path, $pdfContent);
-
-    return Storage::disk('s3')->url($path);
-});
