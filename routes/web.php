3497975117b3a6754265e4a7e7c2d4eb11c952a9
Rohman <103978344+MohammadRohman@users.noreply.github.com>
2025-06-05 15:05:25 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ Route::post('diagnose/proses', [DiagnoseController::class, 'proses'])->name('dia
 Route::get('/hasil/export', [DiagnoseController::class, 'exportPDF'])->name('hasil.export');
 
 
-Route::get('/cdn-test', function () {
-    $path = 'demo_' . now()->timestamp . '.txt';
-    Storage::disk('s3')->put($path, 'Hello from Laravel!');
-    return Storage::disk('s3')->temporaryUrl($path, now()->addMinutes(5));
-});
+// Route::get('/cdn-test', function () {
+//     $path = 'demo_' . now()->timestamp . '.txt';
+//     Storage::disk('s3')->put($path, 'Hello from Laravel!');
+//     return Storage::disk('s3')->temporaryUrl($path, now()->addMinutes(5));
+// });
 
